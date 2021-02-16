1b5eb6deaa592048317581373d8683d99f9cf0b0
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-15 23:34:26 -0600

    Work to adapt to new setting systems

---
+++ FaceFixer/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Mutagen.Bethesda;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceFixer
+{
+    public record Settings
+    {
+        public HashSet<ModKey> TargetMods = new HashSet<ModKey>();
+    }
+}
