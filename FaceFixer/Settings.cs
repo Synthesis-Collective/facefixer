94a340ddb66f0d03482b9b2ff3f9560f79898025
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-06-09 20:52:55 +0200

    Implement the subjects to fix as setting

--- FaceFixer/Settings.cs
+++ FaceFixer/Settings.cs
@@ -1,4 +1,4 @@
-﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +9,19 @@ namespace FaceFixer
     {
         public List<ModKey> TargetMods = new List<ModKey>();
         public bool PrioritizeBySpecifiedOrder = false;
+        public bool PatchAttackRace = true;
+        public bool PatchDefaultOutfit = false;
+        public bool PatchFaceMorph = true;
+        public bool PatchFaceParts = true;
+        public bool PatchFarAwayModel = true;
+        public bool PatchHairColor = true;
+        public bool PatchHeadParts = true;
+        public bool PatchHeadTexture = true;
+        public bool PatchHeight = true;
+        public bool PatchSleepingOutfit = false;
+        public bool PatchTextureLighting = true;
+        public bool PatchTintLayers = true;
+        public bool PatchWeight = true;
+        public bool PatchWornArmor = true;
     }
 }
